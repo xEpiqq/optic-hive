61d6352f31ee02044b9f66a4bad03eafc1e1a1ba
xepiqq <jaydencrowther@gmail.com>
2024-10-29 10:19:47 -0600

    letsgo

--- src/lib/supabaseAdminClient.js
+++ src/lib/supabaseAdminClient.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://smoyvtkwjpeqavtrnzpe.supabase.co'
-const supabaseServiceRoleKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtb3l2dGt3anBlcWF2dHJuenBlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjgwNzgxMCwiZXhwIjoyMDQyMzgzODEwfQ._eVwl6mcyU4C_rTDF8TJ_3sHnPGkfBJkM5W0meR0qsU'
+
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { PRIVATE_SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
 
 export const supabaseAdmin = createClient(
-    supabaseUrl, 
-    supabaseServiceRoleKey
+    PUBLIC_SUPABASE_URL, 
+    PRIVATE_SUPABASE_SERVICE_ROLE_KEY
 );
--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-const supabaseUrl = 'https://smoyvtkwjpeqavtrnzpe.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtb3l2dGt3anBlcWF2dHJuenBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4MDc4MTAsImV4cCI6MjA0MjM4MzgxMH0.kxsFj7c5y2RAZuF4km4zgTXB1Dfjl_aTtTai4ypkFZU"
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
